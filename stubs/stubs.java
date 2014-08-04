e9b3cc28548723448b54ae503639e56e7916acc0
p1-r-squar3d <p1.r.squar3d14@gmail.com>
2014-08-04 21:21:34 +0800

    Update stubs.java

--- stubs/stubs.java
+++ stubs/stubs.java
@@ -1,1 +1,36 @@
-public vodi
+
+public class stub {
+  
+
+private static void floopThePig(int id, Item it, Class<? extends Item> handlerClass) {
+		
+		Constructor constructor;
+		try {
+			constructor = handlerClass.getConstructor(int.class);
+		} catch (NoSuchMethodException e1) {
+			e1.printStackTrace();
+			return;
+		} catch (SecurityException e1) {
+			e1.printStackTrace();
+			return;
+		}
+
+		try {
+			it = (Item)
+			        constructor.newInstance(id);
+		} catch (InstantiationException e) {
+			e.printStackTrace();
+			return;
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+			return;
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+			return;
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+			return;
+		}
+		System.out.println(it.itemID);
+	}
+}
