ddaf56ab6002e89fd9168c941f9b5e33df4c1fe4
p1-r-squar3d <p1.r.squar3d14@gmail.com>
2014-08-04 21:26:26 +0800

    Update stubs.java

--- stubs/stubs.java
+++ stubs/stubs.java
@@ -2,10 +2,14 @@
 public class stub {
   
 
-private static void floopThePig(int id, Item it, Class<? extends Item> handlerClass) {
-		
+private static void foo(int id, Item it, Class<? extends Item> handlerClass) {
+	
+	/* ClientProxy doesn't like this method hence it throws a NullPointer due to the item not being declared?*/
+	
+		/* access the class which is the handler*/
 		Constructor constructor;
 		try {
+			/* Get constructor params*/
 			constructor = handlerClass.getConstructor(int.class);
 		} catch (NoSuchMethodException e1) {
 			e1.printStackTrace();
@@ -16,6 +20,7 @@ private static void floopThePig(int id, Item it, Class<? extends Item> handlerCl
 		}
 
 		try {
+			/* Set constructors params with item id*/
 			it = (Item)
 			        constructor.newInstance(id);
 		} catch (InstantiationException e) {
@@ -31,6 +36,7 @@ private static void floopThePig(int id, Item it, Class<? extends Item> handlerCl
 			e.printStackTrace();
 			return;
 		}
+		/* prints declared item ID which is successful...*/
 		System.out.println(it.itemID);
 	}
 }
